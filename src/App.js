29f0afab17b83cdca055d28596976dae5b698674
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-04-12 19:02:05 +0100

    added simpson quotes API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
+import React from "react";
 function App() {
+  const [simpsonsQote, setSimpsonsQuote] = React.useState({});
+  function getSimpsonsQuote() {
+    // Make a request for a user with a given ID
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then(function (response) {
+        // handle success
+        setSimpsonsQuote(response.data[0]);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getSimpsonsQuote}>Quote Card</button>
+      <img src={simpsonsQote.image} alt={simpsonsQote.character} />
+      <h1>{simpsonsQote.quote}</h1>
+      <p>{simpsonsQote.character}</p>
     </div>
   );
 }
